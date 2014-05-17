6769e2951f5e4ae4d84955d25a403f347043e79f
Zambotti <zambodred@hotmail.com>
2014-05-17 18:44:11 -0300

    Adicionando aula de Foreach 17/05

--- aula1.php
+++ aula1.php
@@ -2,10 +2,10 @@
 $nome = 'welder';  #atribundo valores para a Variavel
 
 if($nome==='welder'){  #Processo de validação
-    echo 'ok';
+    #echo 'ok';
     }
     else{
-        echo 'nao';
+        #echo 'nao';
     }
     
     #estudar encode
@@ -15,25 +15,41 @@ if($nome==='welder'){  #Processo de validação
         #'sobre'=>'Zambotti'
         'dados' => array(   #este é um array dentro do array sobreNome
             'endereco'=> array(   #este é um array dentro do array dentro do array sobreNome e dentro do Array dados
-                'rua' => 'altemar',
-                'cep'=> '0123',
-                'numero'=> 240,)
+                 'altemar',
+                 '0123',
+                 '240',)
             )
         
     );
-    #var_dump($sobreNome);   #para debugar a Variavel
-    echo 'Rua'.' '.$sobreNome['dados']['endereco']['rua'].' '.$sobreNome['dados']['endereco']['cep'];
+   # var_dump($sobreNome);   #para debugar a Variavel
+    #echo 'Rua'.' '.$sobreNome['dados']['endereco']['rua'].' '.$sobreNome['dados']['endereco']['cep'];
     
   # echo $sobreNome['dados'].$sobreNome['sobre'];
    #criação do Array
     
-    /*$sobreNome = array(
+    /*$sobreNome = array=(
          'welder',
         ' Zambotti'
     );
     
    echo $sobreNome[0].$sobreNome[1];
    #criação do Array*/
+    
+    for($i=0;$i<3;$i++){
+        #echo @$sobreNome['dados']['endereco'][$i];
+        
+        
+    }
+   
+    foreach($sobreNome as &$v){
+       foreach ($v as $teste){
+           echo $teste;
+           foreach ($teste as $result){
+               echo $result;
+           }
+       }
        
- ?>
+    }
+ #      var_dump($sobreNome);
+  ?>
 
